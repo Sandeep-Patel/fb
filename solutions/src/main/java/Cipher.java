64fdac89695c5736a87f8c0f19a611e6c29b6770
“Patel <sandepatel@ebay.com>
2021-06-23 16:38:39 +1000

    Initial

--- solutions/src/main/java/Cipher.java
+++ solutions/src/main/java/Cipher.java
@@ -25,13 +25,6 @@
  * output = nopqrstuvwxyzABCDEFGHIJKLM9012345678
  */
 public class Cipher {
-    public static void main(String[] args) {
-        int rotationFactor = 39;
-        String input = "abcdefghijklmNOPQRSTUVWXYZ0123456789";
-
-        String cipheredText = new Cipher().rotationalCipher(input, rotationFactor);
-        System.out.println(cipheredText);
-    }
 
     private static char rotateCharacter(char c, int rotationFactor) {
         if (c > 96 && c < 123) {
@@ -70,4 +63,62 @@ public class Cipher {
         return new String(o);
 
     }
+
+    // These are the tests we use to determine if the solution is correct.
+    // You can add your own at the bottom, but they are otherwise not editable!
+    int test_case_number = 1;
+    void check(String expected, String output) {
+        boolean result = (expected.equals(output));
+        char rightTick = '\u2713';
+        char wrongTick = '\u2717';
+        if (result) {
+            System.out.println(rightTick + " Test #" + test_case_number);
+            System.out.print(rightTick + " Expected: ");
+            printString(expected);
+            System.out.print(" Your output: ");
+            printString(output);
+            System.out.println();
+
+        }
+        else {
+            System.out.print(wrongTick + " Test #" + test_case_number + ": Expected ");
+            printString(expected);
+            System.out.print(" Your output: ");
+            printString(output);
+            System.out.println();
+        }
+        test_case_number++;
+    }
+    void printString(String str) {
+        System.out.print("[\"" + str + "\"]");
+    }
+
+    public void run() {
+        String input_1 = "All-convoYs-9-be:Alert1.";
+        int rotationFactor_1 = 4;
+        String expected_1 = "Epp-gsrzsCw-3-fi:Epivx5.";
+        String output_1 = rotationalCipher(input_1, rotationFactor_1);
+        check(expected_1, output_1);
+
+        String input_2 = "abcdZXYzxy-999.@";
+        int rotationFactor_2 = 200;
+        String expected_2 = "stuvRPQrpq-999.@";
+        String output_2 = rotationalCipher(input_2, rotationFactor_2);
+        check(expected_2, output_2);
+
+        // Add your own test cases here
+
+    }
+
+    public static void main(String[] args) {
+        new Cipher().run();
+    }
+
+    /*public static void main(String[] args) {
+        int rotationFactor = 39;
+        String input = "abcdefghijklmNOPQRSTUVWXYZ0123456789";
+
+        String cipheredText = new Cipher().rotationalCipher(input, rotationFactor);
+        System.out.println(cipheredText);
+    }*/
 }
---
+++ solutions/src/main/java/ContiguousSubArrays.java
@@ -0,0 +1,127 @@
+import java.util.Arrays;
+
+/**
+ * Contiguous Subarrays
+ * You are given an array arr of N integers. For each index i, you are required to determine the number of contiguous
+ * subarrays that fulfill the following conditions:
+ *
+ * The value at index i must be the maximum element in the contiguous subarrays, and
+ * These contiguous subarrays must either start from or end on index i.
+ *
+ * Signature
+ * int[] countSubarrays(int[] arr)
+ *
+ * Input
+ * Array arr is a non-empty list of unique integers that range between 1 to 1,000,000,000
+ * Size N is between 1 and 1,000,000
+ *
+ * Output
+ * An array where each index i contains an integer denoting the maximum number of contiguous subarrays of arr[i]
+ *
+ * Example:
+ * arr = [3, 4, 1, 6, 2]
+ * output = [1, 3, 1, 5, 1]
+ *
+ * Explanation:
+ * For index 0 - [3] is the only contiguous subarray that starts (or ends) with 3, and the maximum value in this subarray is 3.
+ * For index 1 - [4], [3, 4], [4, 1]
+ * For index 2 - [1]
+ * For index 3 - [6], [6, 2], [1, 6], [4, 1, 6], [3, 4, 1, 6]
+ * For index 4 - [2]
+ * So, the answer for the above input is [1, 3, 1, 5, 1]
+ */
+
+// Add any extra import statements you may need here
+
+
+class ContiguousSubArrays {
+
+    // Add any helper functions you may need here
+
+    int[] countSubarrays(int[] arr) {
+
+        if (arr == null || arr.length <= 0) {
+            return new int[0];
+        }
+        int[] res = new int[arr.length];
+        Arrays.fill(res, 1);
+        for (int i = 0; i < arr.length; i++) {
+            res[i] += countSubarrays(arr, i, 1);
+            res[i] += countSubarrays(arr, i, -1);
+        }
+        return res;
+    }
+
+    private int countSubarrays(int[] a, int i, int step) {
+        int s = 0;
+        int m = a[i];
+        i += step;
+        while (i >= 0 && i < a.length) {
+            if (a[i] < m) {
+                s++;
+            } else {
+                break;
+            }
+            i += step;
+        }
+        return s;
+    }
+
+    // These are the tests we use to determine if the solution is correct.
+    // You can add your own at the bottom, but they are otherwise not editable!
+    int test_case_number = 1;
+    void check(int[] expected, int[] output) {
+        int expected_size = expected.length;
+        int output_size = output.length;
+        boolean result = true;
+        if (expected_size != output_size) {
+            result = false;
+        }
+        for (int i = 0; i < Math.min(expected_size, output_size); i++) {
+            result &= (output[i] == expected[i]);
+        }
+        char rightTick = '\u2713';
+        char wrongTick = '\u2717';
+        if (result) {
+            System.out.println(rightTick + " Test #" + test_case_number);
+        }
+        else {
+            System.out.print(wrongTick + " Test #" + test_case_number + ": Expected ");
+            printIntegerArray(expected);
+            System.out.print(" Your output: ");
+            printIntegerArray(output);
+            System.out.println();
+        }
+        test_case_number++;
+    }
+    void printIntegerArray(int[] arr) {
+        int len = arr.length;
+        System.out.print("[");
+        for(int i = 0; i < len; i++) {
+            if (i != 0) {
+                System.out.print(", ");
+            }
+            System.out.print(arr[i]);
+        }
+        System.out.print("]");
+    }
+    public void run() {
+
+        int[] test_1 = {3, 4, 1, 6, 2};
+        int[] expected_1 = {1, 3, 1, 5, 1};
+        int[] output_1 = countSubarrays(test_1);
+        check(expected_1, output_1);
+
+        int[] test_2 = {2, 4, 7, 1, 5, 3};
+        int[] expected_2 = {1, 2, 6, 1, 3, 1};
+        int[] output_2 = countSubarrays(test_2);
+        check(expected_2, output_2);
+
+        // Add your own test cases here
+
+    }
+    public static void main(String[] args) {
+        new ContiguousSubArrays().run();
+    }
+}
+
